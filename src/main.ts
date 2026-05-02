3bf0d172bfb7cdac9e4a0f8241f5126935fdc2b6
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2026-05-02 10:34:14 -0100

    fix: change default port from 3000 to 8080 in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -48,6 +48,6 @@ async function bootstrap() {
   // TypeORM connection pool will be properly closed on graceful shutdown.
   app.enableShutdownHooks();
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
